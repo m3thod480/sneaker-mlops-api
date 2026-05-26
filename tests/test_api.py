06ee14e00ef796eaa31509fa3e238dae13ea2442
m3thod480 <jjbach99@gmail.com>
2026-05-26 02:36:11 +0200

    Se agrega modelo desde S3

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,19 +12,19 @@ def test_health_endpoint():
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
 
-# def test_predict_endpoint():
-#     image_path = Path("tests/test_images/airmax97.jpg")
+def test_predict_endpoint():
+    image_path = Path("tests/test_images/airmax97.jpg")
 
-#     with open(image_path, "rb") as image_file:
-#         response = client.post(
-#             "/predict",
-#             files={"file": ("airmax97.jpg", image_file, "image/jpeg")}
-#         )
+    with open(image_path, "rb") as image_file:
+        response = client.post(
+            "/predict",
+            files={"file": ("airmax97.jpg", image_file, "image/jpeg")}
+        )
 
-#     data = response.json()
+    data = response.json()
 
-#     assert response.status_code == 200
-#     assert "predicted_class" in data
-#     assert "confidence" in data
-#     assert "top_predictions" in data
-#     assert isinstance(data["top_predictions"], list)
+    assert response.status_code == 200
+    assert "predicted_class" in data
+    assert "confidence" in data
+    assert "top_predictions" in data
+    assert isinstance(data["top_predictions"], list)
